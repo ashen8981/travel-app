881e685f384c7b36a13b67bf649e669b130a7d77
Ashen Malindu <ashenmalindu112@gmail.com>
2025-04-07 11:39:06 +0530

    app js edited

--- src/App.js
+++ src/App.js
@@ -1,17 +1,9 @@
+import Topbar from "./components/TopBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>Learn React</h2>
-        </a>
-      </header>
+    <div>
+      <Topbar />
     </div>
   );
 }
