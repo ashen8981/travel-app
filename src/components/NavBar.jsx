d5ec21c1e81caab1f2909fa1863e1a31b59724a9
Ashen Malindu <ashenmalindu112@gmail.com>
2025-04-09 15:57:07 +0530

    navbbar updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,7 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {FaBars} from 'react-icons/fa'
+import {BsChatSquareDots} from 'react-icons/bs'
+import {FaFacebookF,FaTwitter,FaGooglePlusG,FaInstagram} from 'react-icons/fa'
 
 function NavBar() {
   return (
